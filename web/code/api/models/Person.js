a22c6a42b6c9d3b34df4147342f146b2cafd9051
atduarte <mail@andreduarte.net>
2014-05-15 17:49:29 +0100

    Web

---
+++ web/code/api/models/Person.js
@@ -0,0 +1,16 @@
+/**
+ * Person
+ *
+ * @module      :: Model
+ * @description :: A short summary of how this model works and what it represents.
+ * @docs		:: http://sailsjs.org/#!documentation/models
+ */
+
+ var Person = {
+  attributes: {
+    firstName: 'STRING',
+    lastName: 'STRING'
+  }
+};
+
+module.exports = Person;
