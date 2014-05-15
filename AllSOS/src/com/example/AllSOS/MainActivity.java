c28b761ed40695fa76c60225d0ec3e609667438d
smsesteves <sergiomanuelesteves@gmail.com>
2014-05-15 17:02:26 +0100

    First Commit

---
+++ AllSOS/src/com/example/AllSOS/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.AllSOS;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.telephony.TelephonyManager;
+import android.text.Layout;
+import android.view.TextureView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+
+
+    }
+}
